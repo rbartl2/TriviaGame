1f27f569921a75110e4963cca31da84d60e27af2
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-08-20 13:32:58 -0500

    started js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,85 @@
-alert("we are linked");
+$(document).ready(function(){
+
+
+    var correct = 0;
+    var incorrect =0;
+    var clock = 20;
+
+
+
+
+//Create an object of all questions, choices, and answers
+
+    var questionsObject = {
+
+        questions: [
+            "Miley Cyrus most famously starred in which Disney Channel original show?",
+            "Which of these things would a 'basic girl' like?",
+            "What was the name of everyone's first 'friend' on MySpace?",
+            "What does it mean to 'spill the tea'?",
+            "What was the name of the host of 'Blue's Clues?",
+            "Which show wasn't predominantly about wealthy teens?",
+            "What college did Rory choose to attend in 'Gilmore Girls'?",
+            "Which is not the name of one of Kris Jenner's grandkids?",
+            "Which cartoon was not on Nickelodeon?",
+            "Which of these Nickelodeon stars was not an original cast member on 'All That'?"
+
+        ],
+
+        choices: [
+            
+            ["Lizzie McGuire", "Hannah Montana", "Wizards of Waverly Place", "Even Stevens"],
+            ["Pumpkin Spice lattes", "UGGs", "Yoga Pants", "All of the above, obvi"],
+            ["Tom", "Tim", "Toby", "Dave"],
+            ["Gossip is being shared", "To make a mess of your life", "It's a euphemism for something unmentionable", "You literally spilled some tea"],
+            ["Sean", "Shane", "Steve", "Sheldon"],
+            ["Gossip Girl", "Friday Night Lights", "The O.C.", "Laguna Beach"],
+            ["Princeton", "Harvard", "Comumbia", "Yale"],
+            ["Bear", "Stormi", "Chicago", "Reign"],
+            ["Rocket Power", "Rugrats", "Recess", "Rocko's Modern Life"],
+            ["Kel Mitchell", "Jamie Lynn Spears", "Lori Beth Denberg", "Kenan Thompson"],
+
+        ],
+
+        answers: ["Hannah Montana", "All of the above, obvi", "Tom", "Gossip is being shared", "Steve", "Friday Night Lights", "Yale", "Bear", "Recess", "Jamie Lynn Spears"],
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
